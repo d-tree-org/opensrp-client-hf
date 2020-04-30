d849bd26011c783f4d0fbdad09aed7fd21c7a222
Isaiah <issyzac.iz@gmail.com>
2020-04-30 14:18:46 +0300

    added referral option in menu

--- app/src/main/java/org/smartregister/hf/util/Constants.java
+++ app/src/main/java/org/smartregister/hf/util/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
         public static final String PNC = "PNC";
         public static final String FAMILY_PLANNING = "Family Planning";
         public static final String MALARIA = "Malaria";
+        public static final String REFERRALS = "Referrals";
     }
 
     public static final class PEER_TO_PEER {
