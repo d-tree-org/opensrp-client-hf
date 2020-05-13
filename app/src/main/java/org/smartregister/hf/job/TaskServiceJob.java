ddfcc5f2aa4859fac35cf0937ef69ee00bc4ee09
Isaiah <issyzac.iz@gmail.com>
2020-05-13 15:34:13 +0300

    added tasks service job to handle syncing tasks

---
+++ app/src/main/java/org/smartregister/hf/job/TaskServiceJob.java
@@ -0,0 +1,29 @@
+package org.smartregister.hf.job;
+
+import android.content.Intent;
+
+import com.evernote.android.job.Job;
+
+import org.smartregister.AllConstants;
+import org.smartregister.hf.sync.SyncTaskIntentService;
+import org.smartregister.job.BaseJob;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Author : Isaya Mollel on 2020-05-13.
+ */
+public class TaskServiceJob extends BaseJob {
+
+    public static final String TAG = "TaskServiceJob";
+
+    @NonNull
+    @Override
+    protected Job.Result onRunJob(@NonNull Job.Params params) {
+        Intent intent = new Intent(getApplicationContext(), SyncTaskIntentService.class);
+        getApplicationContext().startService(intent);
+        return params != null && params.getExtras().getBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE,
+                false) ? Job.Result.RESCHEDULE : Job.Result.SUCCESS;
+    }
+
+}
