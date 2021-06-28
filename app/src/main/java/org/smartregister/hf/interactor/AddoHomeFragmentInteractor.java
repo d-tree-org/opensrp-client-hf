f88af89d09b4ff9ffda0d42c05e725545cfb3017
issyzac <issyzac.iz@gmail.com>
2021-06-28 23:11:05 +0300

    Implemented home screen interactor to fetch data through

--- app/src/main/java/org/smartregister/hf/interactor/AddoHomeFragmentInteractor.java
+++ app/src/main/java/org/smartregister/hf/interactor/AddoHomeFragmentInteractor.java
@@ -1,12 +1,25 @@
 package org.smartregister.hf.interactor;
 
 import org.smartregister.hf.contract.AddoHomeContract;
+import org.smartregister.hf.contract.AddoHomeFragmentContract;
+import org.smartregister.hf.model.DashboardDataModel;
+import org.smartregister.hf.util.TaksUtils;
 
 import java.util.Date;
 
-public class AddoHomeInteractor implements AddoHomeContract.Interactor {
+public class AddoHomeFragmentInteractor implements AddoHomeFragmentContract.Interactor {
+
     @Override
-    public Date getLastSync() {
-        return null;
+    public void fetchDashboardData(AddoHomeFragmentContract.InteractorCallback callback) {
+
+        int lastThreeDaysReferralCount = TaksUtils.getLastThreeDaysTotalReferralCount();
+        int attended = TaksUtils.getAttendedReferrals();
+
+        DashboardDataModel dashboardData = new DashboardDataModel();
+        dashboardData.setLastThreeDaysReferralCount(lastThreeDaysReferralCount);
+        dashboardData.setReferralsAttendedTodayCount(attended);
+
+        callback.onDashboardDataFetched(dashboardData);
+
     }
 }
