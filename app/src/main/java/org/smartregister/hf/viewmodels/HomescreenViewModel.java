15aba20704c2024c74e8753e646a219ff92a6b41
issyzac <issyzac.iz@gmail.com>
2021-08-19 02:18:12 +0300

    Created viewmodel class for fetching homescreen data

---
+++ app/src/main/java/org/smartregister/hf/viewmodels/HomescreenViewModel.java
@@ -0,0 +1,45 @@
+package org.smartregister.hf.viewmodels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import org.smartregister.hf.contract.AddoHomeFragmentContract;
+import org.smartregister.hf.interactor.AddoHomeFragmentInteractor;
+import org.smartregister.hf.model.DashboardDataModel;
+
+public class HomescreenViewModel extends ViewModel {
+
+    private final MutableLiveData<String> selectedVillage = new MutableLiveData<String>();
+
+    private MutableLiveData<DashboardDataModel> metrics;
+
+    public HomescreenViewModel() {}
+
+    public void setSelectedVillage(String village) {
+        selectedVillage.setValue(village);
+    }
+
+    public LiveData<String> getSelectedVillage() {
+        return selectedVillage;
+    }
+
+    public LiveData<DashboardDataModel> getMetrics() {
+        if (metrics == null){
+            loadAttendedReferrals();
+        }
+        return metrics;
+    }
+
+    private void loadAttendedReferrals(){
+        AddoHomeFragmentInteractor interactor = new AddoHomeFragmentInteractor();
+        metrics  = new MutableLiveData<>();
+        interactor.fetchDashboardData(new AddoHomeFragmentContract.InteractorCallback() {
+            @Override
+            public void onDashboardDataFetched(DashboardDataModel dashboardData) {
+                metrics.setValue(dashboardData);
+            }
+        });
+    }
+
+}
