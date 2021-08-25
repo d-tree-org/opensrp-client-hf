fa39fea2d57435f0bfd36779f7dd95bdfee24078
issyzac <issyzac.iz@gmail.com>
2021-08-25 13:53:30 +0300

    Added report dashboard contract

---
+++ app/src/main/java/org/smartregister/hf/contract/ReportsDashboardContract.java
@@ -0,0 +1,11 @@
+package org.smartregister.hf.contract;
+
+public interface ReportsDashboardContract {
+
+    public interface View {}
+
+    public interface Interactor {}
+
+    public interface Presenter {}
+
+}
