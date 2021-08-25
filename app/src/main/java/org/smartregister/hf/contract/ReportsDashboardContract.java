b93d32504a1254d4a5d2205342df635afc0eee3a
issyzac <issyzac.iz@gmail.com>
2021-08-25 15:12:23 +0300

    Added contract methods

--- app/src/main/java/org/smartregister/hf/contract/ReportsDashboardContract.java
+++ app/src/main/java/org/smartregister/hf/contract/ReportsDashboardContract.java
@@ -1,11 +1,52 @@
 package org.smartregister.hf.contract;
 
+import android.view.ViewGroup;
+
+import org.smartregister.reporting.domain.IndicatorQuery;
+import org.smartregister.reporting.domain.IndicatorTally;
+import org.smartregister.reporting.domain.ReportIndicator;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ReportsDashboardContract {
 
-    public interface View {}
+    public interface View {
 
-    public interface Interactor {}
+        void refreshUI();
 
-    public interface Presenter {}
+        void buildVisualization(ViewGroup viewGroup);
+
+        List<Map<String, IndicatorTally>> getIndicatorTallies();
+
+        void setIndicatorTallies(List<Map<String, IndicatorTally>> tallies);
+
+    }
+
+    public interface Interactor {
+        void scheduleDailyTallyJob();
+    }
+
+    public interface Model {
+        void addIndicator(ReportIndicator indicator);
+
+        void addIndicatorQuery(IndicatorQuery query);
+
+        List<Map<String, IndicatorTally>> getIndicatorsDailyTallies();
+    }
+
+    public interface Presenter {
+
+        void onResume();
+
+        List<Map<String, IndicatorTally>> fetchIndicatorsDailytallies();
+
+        void addIndicators(List<ReportIndicator> indicators);
+
+        void addIndicatorQueries(List<IndicatorQuery> queries);
+
+        void scheduleRecurringTallyJob();
+
+    }
 
 }
