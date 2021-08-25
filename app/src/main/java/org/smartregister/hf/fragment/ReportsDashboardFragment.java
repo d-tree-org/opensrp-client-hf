9b0bcf6ab75a5591ff401da6a1a8a954c95dbe34
issyzac <issyzac.iz@gmail.com>
2021-08-25 15:13:00 +0300

    Implemeted required methods and added first report indicator

--- app/src/main/java/org/smartregister/hf/fragment/ReportsDashboardFragment.java
+++ app/src/main/java/org/smartregister/hf/fragment/ReportsDashboardFragment.java
@@ -9,8 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.smartregister.hf.util.ChartUtils;
+import org.smartregister.reporting.domain.IndicatorQuery;
+import org.smartregister.reporting.domain.IndicatorTally;
+import org.smartregister.reporting.domain.ReportIndicator;
+import org.smartregister.domain.Task;
 import org.smartregister.hf.R;
 import org.smartregister.hf.contract.ReportsDashboardContract;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ReportsDashboardFragment extends Fragment implements ReportsDashboardContract.View {
 
@@ -20,7 +29,42 @@ public class ReportsDashboardFragment extends Fragment implements ReportsDashboa
 
         /**
          * Initialize all the reporting data here
-         */
+         *
+
+        String allReferralsIssuedW = "select * from task " +
+                "where status = '"+ Task.TaskStatus.READY +"' and " +
+                "business_status = 'Referred' and " +
+                "datetime(authored_on/1000, 'unixepoch') > date('now', 'start of day', '-2 days')";
+        */
+
+        addReportIndicators();
+
+    }
+
+
+    private void addReportIndicators(){
+        List<ReportIndicator> reportIndicators = new ArrayList<>();
+        List<IndicatorQuery> indicatorQueries = new ArrayList<>();
+
+        String allReferralsIssuedW = "select * from task where business_status = 'Referred' ";
+
+        ReportIndicator allReferralsIndicator = new ReportIndicator();
+        allReferralsIndicator.setKey(ChartUtils.allReferrals);
+        allReferralsIndicator.setDescription("All Referrals Issued to facility within the ward");
+        reportIndicators.add(allReferralsIndicator);
+
+
+        IndicatorQuery allReferralsIndicatorQuery = new IndicatorQuery();
+        allReferralsIndicatorQuery.setIndicatorCode(ChartUtils.allReferrals);
+        allReferralsIndicatorQuery.setDbVersion(0);
+        allReferralsIndicatorQuery.setId(null);
+        allReferralsIndicatorQuery.setQuery(allReferralsIssuedW);
+        indicatorQueries.add(allReferralsIndicatorQuery);
+
+        //Add Indicators to the presenter
+
+
+
     }
 
     @Nullable
@@ -39,6 +83,26 @@ public class ReportsDashboardFragment extends Fragment implements ReportsDashboa
          * Work with the UI elements once they are created
          *
          */
+
+    }
+
+    @Override
+    public void refreshUI() {
+
+    }
+
+    @Override
+    public void buildVisualization(ViewGroup viewGroup) {
+
+    }
+
+    @Override
+    public List<Map<String, IndicatorTally>> getIndicatorTallies() {
+        return null;
+    }
+
+    @Override
+    public void setIndicatorTallies(List<Map<String, IndicatorTally>> tallies) {
 
     }
 }
