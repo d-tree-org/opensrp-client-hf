5881e5d43f7323fb579582f7331b08ed6029da5b
issyzac <issyzac.iz@gmail.com>
2021-08-26 15:08:36 +0300

    refactored

--- app/src/main/java/org/smartregister/hf/util/ChartUtils.java
+++ app/src/main/java/org/smartregister/hf/util/ChartUtils.java
@@ -5,6 +5,7 @@ public class ChartUtils {
     /// Current Month Indicators
     public static String allReferrals = "IND_ALL_REFERRALS";
     public static String allAttendedReferrals = "IND_ALL_ATTENDED_REFERRALS";
+    public static String currentMonthAttendedReferrals = "IND_THIS_MONTH_ATTENDED_REFERRALS";
 
 
 }
