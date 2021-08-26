0f434c0988c491f76cdf4227750c4264e45aeedb
issyzac <issyzac.iz@gmail.com>
2021-08-26 14:36:00 +0300

    Added attended referrals indicator key

--- app/src/main/java/org/smartregister/hf/util/ChartUtils.java
+++ app/src/main/java/org/smartregister/hf/util/ChartUtils.java
@@ -4,6 +4,7 @@ public class ChartUtils {
 
     /// Current Month Indicators
     public static String allReferrals = "IND_ALL_REFERRALS";
+    public static String allAttendedReferrals = "IND_ALL_ATTENDED_REFERRALS";
 
 
 }
