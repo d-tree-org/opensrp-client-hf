dd1a2fe1c9fe99337d14ecaedf3dab907e6880b7
maulidkategile <maulidkategile@gmail.com>
2022-04-12 11:35:43 +0300

    impliment environment switching when app launch for the first time

--- app/src/main/java/org/smartregister/hf/activity/LoginActivity.java
+++ app/src/main/java/org/smartregister/hf/activity/LoginActivity.java
@@ -1,43 +1,17 @@
 package org.smartregister.hf.activity;
 
-import static java.sql.DriverManager.println;
-
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Button;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.DialogFragment;
-
-import org.json.JSONObject;
-import org.smartregister.AllConstants;
-import org.smartregister.hf.BuildConfig;
 import org.smartregister.hf.R;
 import org.smartregister.hf.fragment.SwitchEnvironmentFragment;
 import org.smartregister.hf.presenter.LoginPresenter;
 import org.smartregister.hf.util.Constants;
-import org.smartregister.repository.AllSharedPreferences;
-import org.smartregister.util.Utils;
 import org.smartregister.view.activity.BaseLoginActivity;
 import org.smartregister.view.contract.BaseLoginContract;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-
-import timber.log.Timber;
 
 /**
  * Author : Isaya Mollel on 2019-10-18.
@@ -60,7 +34,6 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setServerURL();
-//        checkEnvironment();
     }
 
     @Override
@@ -98,29 +71,15 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
     }
 
     public void setServerURL() {
-        AllSharedPreferences preferences = Utils.getAllSharedPreferences();
         try {
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
             String env = sharedPreferences.getString(Constants.ENVIRONMENT_CONFIG.OPENSRP_HF_ENVIRONMENT,"");
             if(env.isEmpty()){
-                Timber.e("Environment %S",env);
                 SwitchEnvironmentFragment dialog = new SwitchEnvironmentFragment();
                 dialog.show(this.getSupportFragmentManager(), "SwitchEnvironmentFragment");
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    private void setButtonIndicator(String env) {
-        if (env.equals(Constants.ENVIRONMENT_CONFIG.TEST_ENVIROMENT)) {
-            //We are on test env
-            Button loginButton = findViewById(R.id.login_login_btn);
-            loginButton.setBackgroundColor(getResources().getColor(R.color.test_environment_color));
-        } else {
-            //we are in production
-            Button loginButton = findViewById(R.id.login_login_btn);
-            loginButton.setBackgroundColor(getResources().getColor(R.color.primary_color));
         }
     }
 }
--- app/src/main/java/org/smartregister/hf/fragment/SwitchEnvironmentFragment.java
+++ app/src/main/java/org/smartregister/hf/fragment/SwitchEnvironmentFragment.java
@@ -3,24 +3,33 @@ package org.smartregister.hf.fragment;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import org.smartregister.AllConstants;
+import org.smartregister.hf.BuildConfig;
 import org.smartregister.hf.R;
+import org.smartregister.hf.util.Constants;
+import org.smartregister.repository.AllSharedPreferences;
+import org.smartregister.util.Utils;
 
 public class SwitchEnvironmentFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.AlertDialogTheme);
+        final String[] listOfEnvironment = new String[]{"Production", "Test"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AlertDialogTheme);
         builder.setTitle("Select the environment you want to switch to");
         builder.setSingleChoiceItems(R.array.hf_environment, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-//                setSelectedEnvironment(listOfEnvironment[which]);
+                setSelectedEnvironment(listOfEnvironment[which]);
                 dialog.dismiss();
             }
         });
@@ -28,9 +37,35 @@ public class SwitchEnvironmentFragment extends DialogFragment {
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-//                setDefaultEnvironment();
+                configureUrl(Constants.ENVIRONMENT_CONFIG.PRODUCTION_ENVIROMENT, BuildConfig.opensrp_url_production);
             }
         });
         return builder.create();
+    }
+
+    private void setSelectedEnvironment(String selectedEnvironment) {
+        if (selectedEnvironment.equalsIgnoreCase(Constants.ENVIRONMENT_CONFIG.PRODUCTION_ENVIROMENT)) {
+            configureUrl(Constants.ENVIRONMENT_CONFIG.PRODUCTION_ENVIROMENT, BuildConfig.opensrp_url_production);
+        } else {
+            configureUrl(Constants.ENVIRONMENT_CONFIG.TEST_ENVIROMENT, BuildConfig.opensrp_url_staging);
+        }
+    }
+
+    private void configureUrl(String env, String baseUrl) {
+        AllSharedPreferences preferences = Utils.getAllSharedPreferences();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        preferences.savePreference(AllConstants.DRISHTI_BASE_URL, baseUrl);
+        sharedPreferences.edit().putBoolean(Constants.ENVIRONMENT_CONFIG.PREFERENCE_PRODUCTION_ENVIRONMENT_SWITCH, true).apply();
+        preferences.savePreference(Constants.ENVIRONMENT_CONFIG.OPENSRP_HF_ENVIRONMENT, env);
+        setButtonIndicator(env);
+    }
+
+    private void setButtonIndicator(String env) {
+        Button loginButton = getActivity().findViewById(R.id.login_login_btn);
+        if (env.equals(Constants.ENVIRONMENT_CONFIG.TEST_ENVIROMENT)) {
+            loginButton.setBackgroundColor(getResources().getColor(R.color.test_environment_color));
+        } else {
+            loginButton.setBackgroundColor(getResources().getColor(R.color.primary_color));
+        }
     }
 }
